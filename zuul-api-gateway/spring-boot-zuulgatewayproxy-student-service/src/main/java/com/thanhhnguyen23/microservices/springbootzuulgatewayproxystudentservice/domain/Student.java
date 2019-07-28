94e5d154a4a4bf2c905938977f2b3478690c5a09
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-07-28 10:12:46 -0500

    adding zuul api/proxy-gateway for student-service

---
+++ zuul-api-gateway/spring-boot-zuulgatewayproxy-student-service/src/main/java/com/thanhhnguyen23/microservices/springbootzuulgatewayproxystudentservice/domain/Student.java
@@ -0,0 +1,28 @@
+package com.thanhhnguyen23.microservices.springbootzuulgatewayproxystudentservice.domain;
+
+public class Student {
+
+    String name;
+    String address;
+    String cls;
+
+
+    public Student(String name, String address, String cls){
+        super();
+        this.name = name;
+        this.address = address;
+        this.cls = cls;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public String getCls(){
+        return cls;
+    }
+}
---
+++ zuul-api-gateway/spring-boot-zuulgatewayproxy-student-service/src/test/java/com/thanhhnguyen23/microservices/springbootzuulgatewayproxystudentservice/SpringBootZuulgatewayproxyStudentServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thanhhnguyen23.microservices.springbootzuulgatewayproxystudentservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootZuulgatewayproxyStudentServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
